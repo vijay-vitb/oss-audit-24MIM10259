4f4a77945dc09cdc50775bfdcd2585e725370c91
VIJAY 24MIM10259 <vijay.24mim10259@vitbhopal.ac.in>
2026-03-29 19:07:43 +0530

    package inspector script for Debian systems
    This script checks if a specified package is installed on Debian-based systems and provides a description of the package.

---
+++ script2.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+# Script 2: FOSS Package Inspector
+# Author: Vijay | Course: Open Source Software
+PACKAGE="git"
+echo "Checking package: $PACKAGE"
+echo "----------------------------"
+# Check if package is installed (Debian-based systems)
+if dpkg -l | grep -qw $PACKAGE; then
+    echo "$PACKAGE is installed."
+    dpkg -s $PACKAGE | grep -E 'Version|Maintainer|Description'
+else
+    echo "$PACKAGE is NOT installed."
+fi
+echo ""
+echo "Package Description:"
+echo "----------------------------"
+# Case statement for description
+case $PACKAGE in
+    git)
+        echo "Git: A distributed version control system that enables collaboration and tracks changes in code."
+        ;;
+    apache2)
+        echo "Apache: A powerful open-source web server used to host websites."
+        ;;
+    mysql-server)
+        echo "MySQL: A relational database system used in web applications."
+        ;;
+    vlc)
+        echo "VLC: A multimedia player that supports almost all media formats."
+        ;;
+    *)
+        echo "Unknown package."
+        ;;
+esac
